8d87149c239f3d2b3c8b79cc984b43206f34155b
Ian Gicheha Mbae <mbaegicheha@gmail.com>
2026-09-22 18:09:39 +0300

    Bump versionCode to 4 (v1.0.2) for release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.rank.lexi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
